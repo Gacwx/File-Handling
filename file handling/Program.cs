47b1edcf1c662fa13b7c5edc6adcf257dedc12ef
Gachay <qacayjabrayilov@gmail.com>
2025-01-15 13:53:57 +0400

    first commit

---
+++ file handling/Program.cs
@@ -0,0 +1,81 @@
+﻿class Program1
+{
+    static void Main()
+    {
+        string filePath = "C:\\Users\\Acer\\source\\repos\\file handling\\input.txt";
+
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
+
+
+class Program2
+{
+    static void Main()
+    {
+        string filePath = "out.txt";
+
+        using (StreamWriter writer = new StreamWriter(filePath))
+        {
+            Console.WriteLine("Введите строки для записи в файл. Нажмите Enter на пустой строке для завершения.");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    break;
+                }
+
+                writer.WriteLine(input);
+            }
+        }
+
+        Console.WriteLine($"Данные успешно записаны в файл {filePath}");
+    }
+}
+
+
+
+class Program3
+{
+    static void Main()
+    {
+        string filePath = "C:\\Users\\Acer\\source\\repos\\file handling\\input.txt";
+
+        if (File.Exists(filePath))
+        {
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                int lineCount = 0;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line.Length > 79)
+                    {
+                        line = line.Substring(0, 79);
+                    }
+
+                    Console.WriteLine(line);
+                    lineCount++;
+
+                    if (lineCount == 24)
+                    {
+                        Console.WriteLine("Press Enter to continue...");
+                        Console.ReadLine();
+                        lineCount = 0;
+                    }
+                }
+            }
+        }
+    }
+}
